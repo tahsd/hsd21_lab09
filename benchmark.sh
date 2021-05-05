bc1c94dd16cce8e9612508c454993254e201816d
root <ta.hsd@cmalab.snu.ac.kr>
2021-05-05 14:36:03 +0000

    benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -9,9 +9,9 @@ sudo python eval.py --num_test_images 100 --m_size 64 --v_size 64 --network mlp 
 echo -e '\n=> Accuracy should be 0.97\n'
 
 # Conv Lowering(CPU) - Acc: 1.0
-sudo python eval.py --num_test_images 100 --m_size 4 --v_size 21 --network cnn --run_type cpu
+sudo python eval.py --num_test_images 100 --v_size 32 --network cnn --run_type cpu
 echo -e '\n=> Accuracy should be 1.0\n'
 
-# Conv Lowering(CPU) - Acc: 1.0
+# Conv Lowering(FPGA) - Acc: 1.0
 # sudo python eval.py --num_test_images 100 --m_size 4 --v_size 4 --network cnn --run_type fpga
 # echo -e '\n=> Accuracy should be 1.0'
